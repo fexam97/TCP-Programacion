1a8e4cdf7faa4f9f95eb6da13c61c94482589291
Fernano <fexam97@gmail.com>
2021-04-28 00:06:24 -0300

    more fechas
    feature overload

---
+++ Clase-2/Fecha.h
@@ -0,0 +1,28 @@
+#ifndef FECHA_H
+#define FECHA_H
+
+
+class Fecha
+{
+private:
+	unsigned long diaRel;
+
+	static const int acumuladoMesesNoBisiesto[13];
+	static const int acumuladoMesesBisiesto[13];
+
+	static int diaDelAnio(int dia, int mes, int anio);
+	static int esBisiesto(int anio);
+	static void diaDelAnioADiaMes(int diaDelAnio, int anio, int* d, int* m);
+
+public:
+	Fecha();
+	Fecha(int dia, int mes, int anio);
+
+	Fecha sumarDias(int dias) const;
+
+	void getDMA(int* d, int* m, int* a) const;
+
+};
+
+
+#endif // FECHA_H
---
+++ Clase-2/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+
+#include "Fecha.h"
+
+
+using namespace std;
+
+
+int main()
+{
+	Fecha hoy(17, 4, 2021);
+    Fecha pepito; // objeto pepito de clase fecha
+
+    pepito = hoy.sumarDias(30);
+
+    int dia, mes, anio;
+
+    pepito.getDMA(&dia, &mes, &anio);
+
+    cout << "La fecha suma es: " << setw(2) << fixed << dia << '/' << setw(2) << fixed << mes << '/' << setw(4) << fixed << anio << endl;
+
+    return 0;
+}
